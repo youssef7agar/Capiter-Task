e12bea356401f10214cb898d29879f0a14d2bb57
Youssef Hagar <Youssef7agar24@gmail.com>
2021-02-28 17:56:35 +0200

    moved apiKey to a separate file

--- app/src/main/java/com/example/capiter/network/ApiService.kt
+++ app/src/main/java/com/example/capiter/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.capiter.network
 
+import com.example.capiter.BuildConfig
 import com.example.capiter.network.model.Product
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -7,6 +8,7 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface ApiService {
+    @Headers("x-apikey: ${BuildConfig.API_KEY}")
     @GET("products")
     fun getProducts(@Query("q") page: String): Single<List<Product>>
 }
